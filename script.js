80c752816530a1fe3391c7b5c82b6b82f428bcca
derrickjs <85529367+derrickjs@users.noreply.github.com>
2022-11-23 10:44:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-const rURL = ""
+const rURL = "https://raw.githubusercontent.com/derrickjs/json/main/stuff.json"
+const pokemon = "https://pokeapi.co/api/v2/pokemon/charizard";
+
+
+
+var request = new XMLHttpRequest();
+
+var getPokemon = new XMLHttpRequest();
+
+request.open("GET", rURL);
+request.responseType = "json";
+
+getPokemon.open("GET", pokemon)
+getPokemon.responseType = "json";
+
+
+getPokemon.send();
+
+
+getPokemon.onload = () => {
+    console.log(getPokemon.response);
+    document.querySelector("body").innerHTML += `<img src = "${getPokemon.response.sprites.front_default}"/> `
+}
+
+request.send();
+
+request.onload = () => {
+    classInfo(request.response);
+
+}
+
+function classInfo(data) {
+    for( let d of data){
+        document.querySelector("body").innerHTML += "<h1>" + d.product + ": " + d.price + "</h1>";
+    }
+}
